f781bf98811e427ca2208d20d6231f37cc90326c
Bannon Tanner <bannonta@amazon.com>
2022-09-28 11:18:29 -0500

    working configuration

---
+++ src/example-aws-config.js
@@ -0,0 +1,20 @@
+const awsconfig = {
+  Auth: {
+    region: "XX-XXXX-X",
+    userPoolId: "XX-XXXX-X_AaBC1bcde",
+    userPoolWebClientId: "12ab34cdefg5h6i7jk8901lmno",
+    identityPoolId: "XX-XXXX-X:3a601c64-7f88-4676-8552-c6fe5a1a4d1c",
+  },
+  API: {
+    endpoints: [
+      {
+        name: "apiName",
+        endpoint: "https://abcdefghij.execute-api.XX-XXXX-X.amazonaws.com/dev/",
+        region: "XX-XXXX-X",
+      },
+    ],
+  },
+  ssr: true,
+};
+
+export default awsconfig;
